780a741a002678c891ae2f0ce86ee675e26c5744
pgplarosa <pgplarosa@gmail.com>
2023-03-28 23:28:37 +0800

    added fast api implementation and test

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,75 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+
+from starter.ml.data import process_data
+
+app = FastAPI()
+
+# Import model, encoder and lb
+with open('model/model.pickle', 'rb') as pickle_file:
+    model = pickle.load(pickle_file)
+
+with open('model/encoder.pickle', 'rb') as pickle_file:
+    encoder = pickle.load(pickle_file)
+
+with open('model/label_binarizer.pickle', 'rb') as pickle_file:
+    lb = pickle.load(pickle_file)   
+
+#categorical features
+cat_features = [
+    "workclass",
+    "education",
+    "marital_status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native_country",
+]
+
+# Features to ingest the body from POST
+class Features(BaseModel):
+    age: int = 39
+    workclass: str = 'State-gov'
+    fnlgt: int = 77516
+    education: str = 'Bachelors'
+    education_num: int = 13
+    marital_status: str = 'Never-married'
+    occupation: str = 'Adm-clerical'
+    relationship: str = 'Not-in-family'
+    race: str = 'White'
+    sex: str = 'Male'
+    capital_gain: int = 2174
+    capital_loss: int = 0
+    hours_per_week: int  = 40
+    native_country: str = 'United-States'
+
+
+@app.get("/")
+async def welcome_user():
+    return {"message": 
+            "Greetings! Welcome to this project!"}
+
+@app.post("/predictions")
+def predict_model(features: Features):
+    data = pd.DataFrame(data = features.dict(by_alias=True), index=[0])
+    print(data)
+    X, _, _, _ = process_data(
+                                data,
+                                categorical_features=cat_features,
+                                label=None,
+                                training=False,
+                                encoder=encoder,
+                                lb=lb)
+
+    predict = model.predict(X)
+
+    if predict[0] == 1:
+        predict = "Salary > 50k"
+    else:
+        predict = "Salary <= 50k"
+    
+    return {'predict': predict}
---
+++ starter/test_main.py
@@ -0,0 +1,37 @@
+from fastapi.testclient import TestClient
+from main import app, Features
+import requests
+
+client = TestClient(app)
+
+def test_api_get_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()['message'] == "Greetings! Welcome to this project!"
+
+def test_target_negative_class():
+    data = Features().dict(by_alias=True)
+    response = client.post("/predictions", json = data)
+    assert response.status_code == 200
+    assert response.json() == {'predict': 'Salary <= 50k'}
+
+def test_target_positive_class():
+    data = Features().dict(by_alias=True)
+    data['age'] = 46
+    data['workclass'] = ' Private'
+    data['fnlgt'] = 117849
+    data['education'] = ' Some-college'
+    data['education_num'] = 10
+    data['marital_status'] = " Married-civ-spouse"
+    data['occupation'] = ' Sales'
+    data['relationship'] = " Husband"
+    data['race'] = ' White'
+    data["sex"] = ' Male'
+    data['capital_gain'] = 15024
+    data['capital_loss'] = 0
+    data['hours_per_week'] = 40
+    data['native-country'] = ' United-States'
+ 
+    response = client.post("/predictions", json = data)
+    assert response.status_code == 200
+    assert response.json() == {'predict': 'Salary > 50k'}
